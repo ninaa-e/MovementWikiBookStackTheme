bffb165efa0fb94b827abc2988436d6533ddbfad
nina <nina.e@systemli.org>
2024-09-17 13:40:15 +0200

    init

---
+++ auth/parts/login-message.blade.php
@@ -0,0 +1,3 @@
+<p class="callout info">
+  If you want editing rights, please reach out to bookstack@iu.uber.space. Thank you!
+</p>
---
+++ auth/parts/register-message.blade.php
@@ -0,0 +1,3 @@
+<p class="callout info">
+  You can try to signup with your activist email. Registration is limited to some E-Mail Providers for Security Reasons. If you want editing rights, please reach out to bookstack@iu.uber.space. Thank you!
+</p>
