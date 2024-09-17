421c3f83e5684c48a51ff9fba3265d9a8f8009e8
nina <nina.e@systemli.org>
2024-09-17 14:54:41 +0200

    new home with books

--- home/default.blade.php
+++ home/default.blade.php
@@ -1,7 +1,8 @@
 @extends('layouts.simple')
 
-@section('body')
+@extends('layouts.tri')
 
+@section('body')
     <div class="container px-xl py-s flex-container-row gap-l wrap justify-space-between">
         <div>
             <div class="icon-list inline block">
@@ -21,6 +22,31 @@
 
     </div>
 
-    
 
+    @include('books.parts.list', ['books' => $books, 'view' => $view])
+@stop
+
+@section('left')
+    @include('home.parts.sidebar')
+@stop
+
+@section('right')
+    <div class="actions mb-xl">
+        <h5>{{ trans('common.actions') }}</h5>
+        <div class="icon-list text-link">
+            @if(user()->can('book-create-all'))
+                <a href="{{ url("/create-book") }}" class="icon-list-item">
+                    <span>@icon('add')</span>
+                    <span>{{ trans('entities.books_create') }}</span>
+                </a>
+            @endif
+            @include('entities.view-toggle', ['view' => $view, 'type' => 'books'])
+            <a href="{{ url('/tags') }}" class="icon-list-item">
+                <span>@icon('tag')</span>
+                <span>{{ trans('entities.tags_view_tags') }}</span>
+            </a>
+            @include('home.parts.expand-toggle', ['classes' => 'text-link', 'target' => '.entity-list.compact .entity-item-snippet', 'key' => 'home-details'])
+            @include('common.dark-mode-toggle', ['classes' => 'icon-list-item text-link'])
+        </div>
+    </div>
 @stop
